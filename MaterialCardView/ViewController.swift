bc3df32c5c0010106f68e99b5d18cd0edd2d6523
Cem Olcay <ccemolcay@gmail.com>
2015-01-22 11:44:03 +0200

    Initial Commit

---
+++ MaterialCardView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MaterialCardView
+//
+//  Created by Cem Olcay on 22/01/15.
+//  Copyright (c) 2015 Cem Olcay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
