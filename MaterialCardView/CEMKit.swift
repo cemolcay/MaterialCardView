2d87165bbb6e265c16cc661f3667cc4441fd1a71
Cem Olcay <ccemolcay@gmail.com>
2015-09-30 12:41:04 +0300

    Update CEMKit.swift

--- MaterialCardView/CEMKit.swift
+++ MaterialCardView/CEMKit.swift
@@ -1190,52 +1190,6 @@ extension UIColor {
     class func Gray (gray: CGFloat, alpha: CGFloat) -> UIColor {
         return self.RGBAColor(gray, g: gray, b: gray, a: alpha)
     }
-    
-    class func HexColor (hex: String) -> UIColor {
-        var red:   CGFloat = 0.0
-        var green: CGFloat = 0.0
-        var blue:  CGFloat = 0.0
-        var alpha: CGFloat = 1.0
-        
-        var rgba = hex
-        if !rgba.hasPrefix("#") {
-            rgba = "#" + rgba
-        }
-        
-        let index   = rgba.startIndex.advancedBy(1)
-        let hex     = rgba.substringFromIndex(index)
-        let scanner = NSScanner(string: hex)
-        var hexValue: CUnsignedLongLong = 0
-        
-        if scanner.scanHexLongLong(&hexValue) {
-            switch (hex.characters.count) {
-            case 3:
-                red   = CGFloat((hexValue & 0xF00) >> 8)       / 15.0
-                green = CGFloat((hexValue & 0x0F0) >> 4)       / 15.0
-                blue  = CGFloat(hexValue & 0x00F)              / 15.0
-            case 4:
-                red   = CGFloat((hexValue & 0xF000) >> 12)     / 15.0
-                green = CGFloat((hexValue & 0x0F00) >> 8)      / 15.0
-                blue  = CGFloat((hexValue & 0x00F0) >> 4)      / 15.0
-                alpha = CGFloat(hexValue & 0x000F)             / 15.0
-            case 6:
-                red   = CGFloat((hexValue & 0xFF0000) >> 16)   / 255.0
-                green = CGFloat((hexValue & 0x00FF00) >> 8)    / 255.0
-                blue  = CGFloat(hexValue & 0x0000FF)           / 255.0
-            case 8:
-                red   = CGFloat((hexValue & 0xFF000000) >> 24) / 255.0
-                green = CGFloat((hexValue & 0x00FF0000) >> 16) / 255.0
-                blue  = CGFloat((hexValue & 0x0000FF00) >> 8)  / 255.0
-                alpha = CGFloat(hexValue & 0x000000FF)         / 255.0
-            default:
-                print("Invalid RGB string, number of characters after '#' should be either 3, 4, 6 or 8", terminator: "")
-            }
-        } else {
-            print("Scan hex error")
-        }
-        
-        return UIColor (red: red, green:green, blue:blue, alpha:alpha)
-    }
 }
 
 
@@ -1579,7 +1533,7 @@ class BlockButton: UIButton {
         super.init(frame: frame)
     }
     
-    required init?(coder aDecoder: NSCoder) {
+    required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
     
@@ -1611,7 +1565,7 @@ class BlockWebView: UIWebView, UIWebViewDelegate {
         delegate = self
     }
     
-    required init?(coder aDecoder: NSCoder) {
+    required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
     
